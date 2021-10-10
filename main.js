72e58044eb6b7ee7efa0cb726be3af7f228059d1
dominicferrando <dominicf2001@gmail.com>
2021-10-10 19:55:28 -0400

    Add logic to display only one dropdown at a time

---
+++ main.js
@@ -0,0 +1,16 @@
+const allDropdowns = document.querySelectorAll('.dropdown');
+
+function dropdownToggle(selectedDropdown) {
+    selectedDropdown.firstElementChild.classList.toggle('dropdown-summary-flipped');
+    // Closes all other dropdowns
+    allDropdowns.forEach(dropdown => {
+        if (dropdown != selectedDropdown && dropdown.open) {
+            dropdown.firstElementChild.classList.toggle('dropdown-summary-flipped');
+            dropdown.open = false;
+        }
+    });
+}
+
+allDropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', dropdownToggle.bind(null, dropdown));
+});

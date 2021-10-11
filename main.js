484b9134a4560f87b8d7c9fb937bc07ed3fca29a
dominicferrando <dominicf2001@gmail.com>
2021-10-11 11:17:59 -0400

    Add dropdown toggling on window click

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const allDropdowns = document.querySelectorAll('.dropdown');
 
 function dropdownToggle(selectedDropdown) {
     selectedDropdown.firstElementChild.classList.toggle('dropdown-summary-flipped');
+    
     // Closes all other dropdowns
     allDropdowns.forEach(dropdown => {
         if (dropdown != selectedDropdown && dropdown.open) {
@@ -14,3 +15,12 @@ function dropdownToggle(selectedDropdown) {
 allDropdowns.forEach(dropdown => {
     dropdown.addEventListener('click', dropdownToggle.bind(null, dropdown));
 });
+
+window.addEventListener('click', () => {
+    allDropdowns.forEach(dropdown => {
+        if (dropdown.open) {
+            dropdown.firstElementChild.classList.toggle('dropdown-summary-flipped');
+            dropdown.open = false;
+        }
+    });
+})
